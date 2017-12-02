61ebb99b7ee7f968efc101bcb299acca18f672fd
Peter Burns <peterburnshicks@gmail.com>
2017-12-02 16:05:47 -0500

    changed back to popup

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -8,7 +8,7 @@ var config = {
 };
 firebase.initializeApp(config);
 var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithRedirect(provider).then(function(result) {
+firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
     // The signed-in user info.
