b6ad1f2ef8b78fa85912e264838c511de10852ee
Peter Burns <peterburnshicks@gmail.com>
2017-12-02 00:36:59 -0500

    added remove and update buttons

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -29,20 +29,51 @@ $('#newTrain').on('click', function (event) {
     $('#frequency').val('');
 });
 database.ref().on('child_added', function (snapshot) {
+    var key = snapshot.key;
     var train = snapshot.val();
     var name = train.name;
     var destination = train.destination;
     var frequency = train.frequency;
     var fTrainTime = train.fTrainTime;
     var newRow = $('<tr>');
-    newRow.append($('<td>' + name + '</td>'));
-    newRow.append($('<td>' + destination + '</td>'));
-    newRow.append($('<td>' + frequency + '</td>'));
+    newRow.attr('data-key',key);
+    newRow.attr('data-fTrainTime',fTrainTime);
+    newRow.append($('<td contenteditable="true">' + name + '</td>'));
+    newRow.append($('<td contenteditable="true">' + destination + '</td>'));
+    newRow.append($('<td contenteditable="true">' + frequency + '</td>'));
     var firstTime = moment(fTrainTime, "HH:mm").subtract(1, "years");
     var diffTime = moment().diff(firstTime, "minutes");
     var minutesAway = frequency - (diffTime % frequency);
     var nextArrival = moment(moment().add(minutesAway,'minutes')).format("HH:mm");
     newRow.append('<td>' + nextArrival + '</td>');
     newRow.append($('<td>' + minutesAway + '</td>'));
+    newRow.append($('<td><button class="btn btn-sm btn-success updateBtn">Update</button></td>'));
+    newRow.append($('<td><button class="btn btn-sm btn-danger removeBtn">Remove</button></td>'));
     $('#scheduleTable tbody').append(newRow);
+});
+$('body').on('click','.updateBtn',function(){
+    var row = $(this).parent().parent();
+    var key = row.attr('data-key');
+    var name = row.children(':nth-child(1)').text();
+    var destination = row.children(':nth-child(2)').text();
+    var frequency = row.children(':nth-child(3)').text();
+    var keyRef = database.ref().child(key);
+    keyRef.update({
+        name : name,
+        destination : destination,
+        frequency : frequency,
+    });
+    var fTrainTime = row.attr('data-fTrainTime');
+    var firstTime = moment(fTrainTime, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(firstTime, "minutes");
+    var minutesAway = frequency - (diffTime % frequency);
+    var nextArrival = moment(moment().add(minutesAway,'minutes')).format("HH:mm");
+    row.children(':nth-child(4)').text(nextArrival);
+    row.children(':nth-child(5)').text(minutesAway);
+});
+$('body').on('click','.removeBtn',function(){
+    var row = $(this).parent().parent();
+    var keyRef = database.ref().child(row.attr('data-key'));
+    keyRef.remove();
+    row.remove();
 });
